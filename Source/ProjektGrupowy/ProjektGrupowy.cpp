80ce2553d03603bcc8aa60aeb9613e5f1e7a7edc
Dawid <dawbor589@gmail.com>
2026-03-24 17:02:35 +0100

    initial commit

---
+++ Source/ProjektGrupowy/ProjektGrupowy.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjektGrupowy.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjektGrupowy, "ProjektGrupowy" );
